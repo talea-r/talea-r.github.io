164bc8a5b7353d700ba8cf594b773f4d3e6b2e49
Talea <talearendon@Taleas-MacBook-Pro.local>
2019-04-29 10:54:30 -0700

    ellipses

--- p5js/p5.js
+++ p5js/p5.js
@@ -1,6 +1,7 @@
 
 let song;
 var angle = 0;
+var speed = 0.05;
 
 function preload() {
   song = loadSound('music/music.mp3');
@@ -8,20 +9,36 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(171, 203, 254)
+  fill(171, 203, 254);
   song.loop(); // song is ready to play during setup() because it was loaded during preload
   noCursor();
 }
 
 function draw() {
+  // background(255);
+  // translate(mouseX, mouseY);
+  // rotate(radians(angle));
+  // line(-50, -50, 50, 50);
+  // line(50, -50, -50, 50);
+  // line(25, -25, -25, 25);
+  // line(-25, -25, 25, 25);
+  // angle+= 3;
+
+  var l0 = map(mouseX, 0, width, 10, 300);
+
   background(255);
-  translate(mouseX, mouseY);
-  rotate(radians(angle));
-  line(-50, -50, 50, 50);
-  line(50, -50, -50, 50);
-  line(25, -25, -25, 25);
-  line(-25, -25, 25, 25);
-  angle+= 3;
+
+  translate(width/2, height/2);
+  rotate(angle)
+  for(var i=0; i,5; i++) {
+    push();
+    rotate(i+TWO_PI/5);
+    translate(0, l0);
+    ellipse(0, 0, 20, 20);
+    pop();
+  }
+
+  angle += speed;
 }
 
 function mousePressed() {
